0c744bec35a4227220fa1474683ea69d07fa7ca3
perfectSquare <naeembinadam@yahoo.com>
2022-09-21 21:20:43 +0500

    server port for heroku

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ const resolvers = {
     ],
   });
   
-  // The `listen` method launches a web server.
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`
+      🚀  Server is ready at ${url}
+      📭  Query at https://studio.apollographql.com/dev
+    `);
   });
